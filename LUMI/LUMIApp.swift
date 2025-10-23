1189a0c60f91417b641eeba143c051075d2de526
haruni24 <haruni20@icloud.com>
2025-10-23 17:03:09 +0900

    :tada: Start LUMI

---
+++ LUMI/LUMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LUMIApp.swift
+//  LUMI
+//
+//  Created by 西島賢太朗 on 2025/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LUMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LUMITests/LUMITests.swift
@@ -0,0 +1,17 @@
+//
+//  LUMITests.swift
+//  LUMITests
+//
+//  Created by 西島賢太朗 on 2025/10/23.
+//
+
+import Testing
+@testable import LUMI
+
+struct LUMITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
